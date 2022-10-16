021662842910b0698d7eac690e51d66f419fb073
David Ballesteros <david_ballesteros@outlook.com>
2022-10-17 01:53:38 +0800

    Fix brackets

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -32,155 +32,153 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Help & About"),
+      appBar: AppBar(
+        title: const Text("Help & About"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(
+          top: 10.0,
         ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.only(
-            top: 10.0,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    "What is a ZIP Code?",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-                subtitle: Text(
-                  "ZIP (Zone Identification Plan) Codes are a system of postal codes first used by the United States Postal Services (USPS) in 1963 - so that the mail travels more efficiently, and therefore more quickly, when senders use the code in the postal address.\n\nIn the Philippines, the ZIP Code is used by the Philippine Postal Corporation (PhilPost) to simplify the distribution of mail.",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ),
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    "Where to put the ZIP Code?",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                        "The ZIP Code is usually placed at the left-hand corner of the last line where the city/town is written:",
-                        style: Theme.of(context).textTheme.bodyMedium),
-                    Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.only(top: 10.0),
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.0,
-                          color: const Color(0xFFAAAAAA),
-                        ),
-                        color: Colors.brown.shade100,
-                        borderRadius: BorderRadius.circular(5.0),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color(0x33000000),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          for (String text in sampleFormatText)
-                            Text(
-                              text,
-                              style: const TextStyle(
-                                fontSize: 14.0,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              TileIcon(
-                iconData: Icons.share,
-                label: "Share this app",
-                onTap: () {
-                  Share.share(
-                      'Sending out mail or parcel? Or even filling out forms? Look up Philippines ZIP codes in ZIP Code PH app. Download here: https://reddavid.me/zipcodeph-app/',
-                      subject: "Share ZIP Code PH app");
-                },
-              ),
-              TileIcon(
-                iconData: Icons.feedback,
-                label: "Send feedback",
-                onTap: () {
-                  launchUrlString(
-                      "mailto:reddavidapps?subject=[FEEDBACK] ZIP Code PH&body=App version: " +
-                          _packageInfo.version +
-                          " build " +
-                          _packageInfo.buildNumber);
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  top: 20.0,
-                  left: 15.0,
-                  right: 20.0,
-                  bottom: 10.0,
-                ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
                 child: Text(
-                  "Photo/Image Attributions:",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  "What is a ZIP Code?",
+                  style: Theme.of(context).textTheme.headline6,
                 ),
               ),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: Column(
-                  children: [
-                    for (var a in kAttributions)
-                      ListTile(
-                        visualDensity: const VisualDensity(vertical: -4.0),
-                        onTap: () {
-                          launchUrlString(a.last.toString());
-                        },
-                        dense: true,
-                        title: Text(
-                          a.first,
-                          style: const TextStyle(
-                            fontSize: 12.0,
-                          ),
-                        ),
-                      )
-                  ],
+              subtitle: Text(
+                "ZIP (Zone Identification Plan) Codes are a system of postal codes first used by the United States Postal Services (USPS) in 1963 - so that the mail travels more efficiently, and therefore more quickly, when senders use the code in the postal address.\n\nIn the Philippines, the ZIP Code is used by the Philippine Postal Corporation (PhilPost) to simplify the distribution of mail.",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  "Where to put the ZIP Code?",
+                  style: Theme.of(context).textTheme.headline6,
                 ),
               ),
-              Center(
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20.0, horizontal: 10.0),
-                    child: Text(
-                      "version ${_packageInfo.version} build ${_packageInfo.buildNumber}",
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.grey,
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                      "The ZIP Code is usually placed at the left-hand corner of the last line where the city/town is written:",
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(top: 10.0),
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1.0,
+                        color: const Color(0xFFAAAAAA),
                       ),
-                      textAlign: TextAlign.center,
-                    )),
-              )
-            ],
-          ),
-        ));
-  }
-
-  void _launchUrl(String url) async {
-    if (!await launchUrl(Uri.parse(url))) throw 'Could not launch $url';
+                      color: Colors.brown.shade100,
+                      borderRadius: BorderRadius.circular(5.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x33000000),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        for (String text in sampleFormatText)
+                          Text(
+                            text,
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TileIcon(
+              iconData: Icons.share,
+              label: "Share this app",
+              onTap: () {
+                Share.share(
+                    'Sending out mail or parcel? Or even filling out forms? Look up Philippines ZIP codes in ZIP Code PH app. Download here: https://reddavid.me/zipcodeph-app/',
+                    subject: "Share ZIP Code PH app");
+              },
+            ),
+            TileIcon(
+              iconData: Icons.feedback,
+              label: "Send feedback",
+              onTap: () {
+                launchUrlString(
+                    "mailto:reddavidapps?subject=[FEEDBACK] ZIP Code PH&body=App version: " +
+                        _packageInfo.version +
+                        " build " +
+                        _packageInfo.buildNumber);
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 20.0,
+                left: 15.0,
+                right: 20.0,
+                bottom: 10.0,
+              ),
+              child: Text(
+                "Photo/Image Attributions:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              child: Column(
+                children: [
+                  for (var a in kAttributions)
+                    ListTile(
+                      visualDensity: const VisualDensity(vertical: -4.0),
+                      onTap: () {
+                        launchUrlString(a.last.toString());
+                      },
+                      dense: true,
+                      title: Text(
+                        a.first,
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    )
+                ],
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 10.0),
+                child: Text(
+                  "version ${_packageInfo.version} build ${_packageInfo.buildNumber}",
+                  style: const TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
