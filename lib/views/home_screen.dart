e5e7f27610729c13cd12c9fab9993ba25a5d6031
David Ballesteros <david_ballesteros@outlook.com>
2022-11-18 20:05:54 +0800

    adjust home page

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -20,12 +20,12 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(
-        left: 10.0,
-        right: 10.0,
-        top: 20.0,
+        left: 20.0,
+        right: 20.0,
+        top: 30.0,
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           TriviaBox(),
           const SizedBox(height: 20.0),
@@ -35,7 +35,6 @@ class HomeScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   for (AreaMenuButton button in menuButtons) button,
-                  const SizedBox(height: 20.0),
                 ],
               ),
             ),
